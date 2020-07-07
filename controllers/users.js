0dd2a1bbbfc68a98f6c988ef34bfe9b04a26c5ae
Anton Zmanovskii <antonzm34@gmail.com>
2020-07-07 10:01:16 +0400

    захэшировал пароль

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 const user = require('../models/user');
 
 module.exports.getUsers = (req, res) => {
@@ -9,11 +10,18 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.createUser = (req, res) => {
-  const { name, about, avatar } = req.body;
-  user
-    .create({ name, about, avatar })
-    .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: err }));
+  const { name, about, avatar, email, password } = req.body;
+
+  if (validator.isEmail(email)) {
+    bcrypt.hash(password, 10).then((hash) => {
+      user
+        .create({ name, about, avatar, email, password: hash })
+        .then((users) => res.send({ data: users }))
+        .catch((err) => res.status(500).send({ message: err }));
+    });
+  } else {
+    res.status(400).send({ message: 'Проверьте введенные данные' });
+  }
 };
 
 module.exports.getUser = async (req, res) => {
--- models/user.js
+++ models/user.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 const validateUrl = /^(https?:\/\/)((((www\.)?[\w\d](([\w\d.-]+)*)[\w\d]*\.(([a-z]{2,})\.?)+)|(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))(:(?=[1-9])([1-9][0-9]{0,3}|[1-5][0-9]{4}|6[0-4][0-9]{3}|65[0-4][0-9]{2}|655[0-2][0-9]|6553[0-5])?)?)(\/(?!\/)[\w\d]*)*?#?(\.\w{2,})?$/;
-// const validateEmail = /((\w)|(-))+@([a-zA-Z0-9_\-\.]+)\.(([a-zA-Z]{2,})\.?)+/;
 
 const usersSchema = new mongoose.Schema({
   name: {
@@ -26,11 +24,14 @@ const usersSchema = new mongoose.Schema({
     required: true,
   },
   email: {
+    type: String,
     required: true,
     unique: true,
   },
   password: {
+    type: String,
     required: true,
+    minlength: 6,
   },
 });
 
