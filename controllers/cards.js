70af4776a037ea50683e554aeb4db32ed7e00ebf
Anton Zmanovskii <antonzm34@gmail.com>
2020-07-07 16:49:41 +0400

    запретил удалять чужие карточки

--- controllers/cards.js
+++ controllers/cards.js
@@ -15,10 +15,10 @@ module.exports.createCard = (req, res) => {
 };
 
 module.exports.deleteCard = async (req, res) => {
-  const cardObj = await card.findByIdAndRemove(req.params.cardId);
-  if (cardObj == null) {
-    res.status(404).send({ message: 'Карточка не найдена' });
+  const cardObj = await card.findByIdAndDelete(req.params.cardId);
+  if (cardObj == null || !cardObj.owner.toString() === req.user._id) {
+    res.status(404).send({ message: 'У Вас нет такой карточки' });
   } else {
-    res.send({ cardObj });
+    res.send({ data: cardObj, message: 'Карточка удалена' });
   }
 };
